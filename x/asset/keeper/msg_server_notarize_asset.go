2514f20649d19e875eab33684c3667d4f8012bfd
Lorenz Herzberger <lorenzherzberger@gmail.com>
2023-05-23 14:31:03 +0200

    added get/store asset keeper methods
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

--- x/asset/keeper/msg_server_notarize_asset.go
+++ x/asset/keeper/msg_server_notarize_asset.go
@@ -11,14 +11,17 @@ import (
 func (k msgServer) NotarizeAsset(goCtx context.Context, msg *types.MsgNotarizeAsset) (*types.MsgNotarizeAssetResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
-
 	// CHECK IF MSG CREATOR (pub_key) IS ATTESTED MACHINE
+	var asset = types.Asset{
+		Hash:      msg.CidHash,
+		Signature: msg.Sign,
+		Pubkey:    msg.Creator,
+	}
 
-	// CHECK SHORTENED URL FOR NODE
+	// CHECK LOCATION FOR NODE
 
 	// STORE CID_HASH SIGNATURE PUBLIC KEY
+	k.StoreAsset(ctx, asset)
 
 	return &types.MsgNotarizeAssetResponse{}, nil
 }
