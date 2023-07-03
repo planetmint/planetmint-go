181907eeb4844694f51b4cc7ef53c40a547bd52e
Lorenz Herzberger <lorenzherzberger@gmail.com>
2023-07-03 14:40:45 +0200

    ignite scaffold message attest-machine machine:Machine --module machine
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

--- x/machine/module_simulation.go
+++ x/machine/module_simulation.go
@@ -23,7 +23,11 @@ var (
 )
 
 const (
-// this line is used by starport scaffolding # simapp/module/const
+	opWeightMsgAttestMachine = "op_weight_msg_attest_machine"
+	// TODO: Determine the simulation weight value
+	defaultWeightMsgAttestMachine int = 100
+
+	// this line is used by starport scaffolding # simapp/module/const
 )
 
 // GenerateGenesisState creates a randomized GenState of the module.
@@ -51,6 +55,17 @@ func (AppModule) ProposalContents(_ module.SimulationState) []simtypes.WeightedP
 func (am AppModule) WeightedOperations(simState module.SimulationState) []simtypes.WeightedOperation {
 	operations := make([]simtypes.WeightedOperation, 0)
 
+	var weightMsgAttestMachine int
+	simState.AppParams.GetOrGenerate(simState.Cdc, opWeightMsgAttestMachine, &weightMsgAttestMachine, nil,
+		func(_ *rand.Rand) {
+			weightMsgAttestMachine = defaultWeightMsgAttestMachine
+		},
+	)
+	operations = append(operations, simulation.NewWeightedOperation(
+		weightMsgAttestMachine,
+		machinesimulation.SimulateMsgAttestMachine(am.accountKeeper, am.bankKeeper, am.keeper),
+	))
+
 	// this line is used by starport scaffolding # simapp/module/operation
 
 	return operations
@@ -59,6 +74,14 @@ func (am AppModule) WeightedOperations(simState module.SimulationState) []simtyp
 // ProposalMsgs returns msgs used for governance proposals for simulations.
 func (am AppModule) ProposalMsgs(simState module.SimulationState) []simtypes.WeightedProposalMsg {
 	return []simtypes.WeightedProposalMsg{
+		simulation.NewWeightedProposalMsg(
+			opWeightMsgAttestMachine,
+			defaultWeightMsgAttestMachine,
+			func(r *rand.Rand, ctx sdk.Context, accs []simtypes.Account) sdk.Msg {
+				machinesimulation.SimulateMsgAttestMachine(am.accountKeeper, am.bankKeeper, am.keeper)
+				return nil
+			},
+		),
 		// this line is used by starport scaffolding # simapp/module/OpMsg
 	}
 }
