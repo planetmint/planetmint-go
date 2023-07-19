a6814a05940a2e8328f0fec99e6109c54ba0e1ea
Lorenz Herzberger <lorenzherzberger@gmail.com>
2023-07-19 16:21:44 +0200

    add check for machine.reissue == true
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

--- x/machine/keeper/msg_server_attest_machine.go
+++ x/machine/keeper/msg_server_attest_machine.go
@@ -11,6 +11,10 @@ import (
 func (k msgServer) AttestMachine(goCtx context.Context, msg *types.MsgAttestMachine) (*types.MsgAttestMachineResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
+	if msg.Machine.Reissue {
+		k.Logger(ctx).Info("TODO Implement handle on reissue == true")
+	}
+
 	k.StoreMachine(ctx, *msg.Machine)
 	k.StoreMachineIndex(ctx, *msg.Machine)
 
