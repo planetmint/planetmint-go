ce7a73a11670d53e996d7b27a978ef895cbea2d0
Lorenz Herzberger <lorenzherzberger@gmail.com>
2023-09-27 20:26:50 +0200

    add issuePoPRewards and fix typo
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

--- x/dao/keeper/msg_server_report_pop_result.go
+++ x/dao/keeper/msg_server_report_pop_result.go
@@ -4,14 +4,29 @@ import (
 	"context"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/hypebeast/go-osc/osc"
+	"github.com/planetmint/planetmint-go/config"
 	"github.com/planetmint/planetmint-go/x/dao/types"
 )
 
 func (k msgServer) ReportPopResult(goCtx context.Context, msg *types.MsgReportPopResult) (*types.MsgReportPopResultResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	// TODO validate only machine node can be creator
+	k.issuePoPRewards(*msg.Challenge)
+	k.StoreChallenge(ctx, *msg.Challenge)
 
 	return &types.MsgReportPopResultResponse{}, nil
+}
+
+func (k msgServer) issuePoPRewards(challenge types.Challenge) error {
+	cfg := config.GetConfig()
+	client := osc.NewClient(cfg.WatchmenEndpoint, 1234)
+
+	msg := osc.NewMessage("/rddl/token")
+	msg.Append(challenge.Challenger)
+	msg.Append(challenge.Challengee)
+	err := client.Send(msg)
+
+	return err
 }
