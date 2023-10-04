e086ed7680b65a4a5d6d31c72947db9c39259af4
Lorenz Herzberger <lorenzherzberger@gmail.com>
2023-10-04 17:21:49 +0200

    ignite scaffold query get-mint-requests-by-hash hash --response mint-request:MintRequest --module dao
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

--- x/dao/client/cli/query.go
+++ x/dao/client/cli/query.go
@@ -22,6 +22,8 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	}
 
 	cmd.AddCommand(CmdQueryParams())
+	cmd.AddCommand(CmdGetMintRequestsByHash())
+
 	// this line is used by starport scaffolding # 1
 
 	return cmd
