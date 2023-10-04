53ad497b196f19f2fe74381b268478f82fb8f274
Lorenz Herzberger <lorenzherzberger@gmail.com>
2023-10-04 17:02:37 +0200

    add msg validation for mint request
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

--- x/dao/keeper/msg_server_mint_token.go
+++ x/dao/keeper/msg_server_mint_token.go
@@ -3,12 +3,36 @@ package keeper
 import (
 	"context"
 
+	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/planetmint/planetmint-go/config"
 	"github.com/planetmint/planetmint-go/x/dao/types"
 )
 
 func (k msgServer) MintToken(goCtx context.Context, msg *types.MsgMintToken) (*types.MsgMintTokenResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
+	cfg := config.GetConfig()
+
+	amt := msg.GetMintRequest().GetAmount()
+	beneficiary := msg.GetMintRequest().GetBeneficiary()
+
+	coin := sdk.NewCoin(cfg.TokenDenom, sdk.NewIntFromUint64(amt))
+	coins := sdk.NewCoins(coin)
+
+	err := k.bankKeeper.MintCoins(ctx, types.ModuleName, coins)
+	if err != nil {
+		return nil, errorsmod.Wrapf(types.ErrMintFailed, "error while minting %v token for address %s", amt, beneficiary)
+	}
+
+	beneficiaryAddr, err := sdk.AccAddressFromBech32(beneficiary)
+	if err != nil {
+		return nil, errorsmod.Wrapf(types.ErrInvalidAddress, "for provided address %s", beneficiary)
+	}
+
+	err = k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, beneficiaryAddr, coins)
+	if err != nil {
+		return nil, errorsmod.Wrapf(types.ErrTransferFailed, "error while transferring %v token to address %s", amt, beneficiary)
+	}
 
 	k.StoreMintRequest(ctx, *msg.MintRequest)
 
--- x/dao/types/errors.go
+++ x/dao/types/errors.go
@@ -9,4 +9,7 @@ import (
 // x/dao module sentinel errors
 var (
 	ErrInvalidMintAddress = errorsmod.Register(ModuleName, 2, "invalid mint address")
+	ErrMintFailed         = errorsmod.Register(ModuleName, 3, "minting failed")
+	ErrTransferFailed     = errorsmod.Register(ModuleName, 4, "transfer failed")
+	ErrInvalidAddress     = errorsmod.Register(ModuleName, 5, "invalid address")
 )
