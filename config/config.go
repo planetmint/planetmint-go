62fa64453562f95e12c3e3439fa27efbccd0038e
Jürgen Eckel <juergen@riddleandcode.com>
2023-10-04 15:39:49 +0200

    added asset-registration endpoint config added methods to locally issue the asset and register it
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- config/config.go
+++ config/config.go
@@ -13,8 +13,8 @@ const DefaultConfigTemplate = `
 ###############################################################################
 
 [planetmint]
-osc-service-port = {{ .PlmntConfig.OSCServicePort }}
-watchmen-endpoint = "{{ .PlmntConfig.WatchmenEndpoint }}"
+
+asset-registry-endpoint = "{{ .PlmntConfig.AssetRegistryEndpoint }}"
 watchmen-port = {{ .PlmntConfig.WatchmenPort }}
 token-denom = "{{ .PlmntConfig.TokenDenom }}"
 stake-denom = "{{ .PlmntConfig.StakeDenom }}"
@@ -27,16 +27,15 @@ issuance-port = {{ .PlmntConfig.IssuancePort }}
 
 // Config defines Planetmint's top level configuration
 type Config struct {
-	OSCServicePort   int    `mapstructure:"osc-service-port" json:"osc-service-port"`
-	WatchmenEndpoint string `mapstructure:"watchmen-endpoint" json:"watchmen-endpoint"`
-	WatchmenPort     int    `mapstructure:"watchmen-port" json:"watchmen-port"`
-	TokenDenom       string `mapstructure:"token-denom" json:"token-denom"`
-	StakeDenom       string `mapstructure:"stake-denom" json:"stake-denom"`
-	FeeDenom         string `mapstructure:"fee-denom" json:"fee-denom"`
-	ConfigRootDir    string `mapstructure:"config-root-dir" json:"config-root-dir"`
-	PoPEpochs        int    `mapstructure:"pop-epochs" json:"pop-epochs"`
-	IssuanceEndpoint string `mapstructure:"issuance-endpoint" json:"issuance-endpoint"`
-	IssuancePort     int    `mapstructure:"issuance-port" json:"issuance-port"`
+	AssetRegistryEndpoint string `mapstructure:"asset-registry-endpoint " json:"asset-registry-endpoint "`
+	WatchmenPort          int    `mapstructure:"watchmen-port" json:"watchmen-port"`
+	TokenDenom            string `mapstructure:"token-denom" json:"token-denom"`
+	StakeDenom            string `mapstructure:"stake-denom" json:"stake-denom"`
+	FeeDenom              string `mapstructure:"fee-denom" json:"fee-denom"`
+	ConfigRootDir         string `mapstructure:"config-root-dir" json:"config-root-dir"`
+	PoPEpochs             int    `mapstructure:"pop-epochs" json:"pop-epochs"`
+	IssuanceEndpoint      string `mapstructure:"issuance-endpoint" json:"issuance-endpoint"`
+	IssuancePort          int    `mapstructure:"issuance-port" json:"issuance-port"`
 }
 
 // cosmos-sdk wide global singleton
@@ -53,16 +52,15 @@ func DefaultConfig() *Config {
 	}
 
 	return &Config{
-		OSCServicePort:   8766,
-		WatchmenEndpoint: "lab.r3c.network",
-		WatchmenPort:     7401,
-		TokenDenom:       "plmnt",
-		StakeDenom:       "plmntstake",
-		FeeDenom:         "plmnt",
-		ConfigRootDir:    filepath.Join(currentUser.HomeDir, ".planetmint-go"),
-		PoPEpochs:        24, // 24 CometBFT epochs of 5s equate 120s
-		IssuanceEndpoint: "lab.r3c.network",
-		IssuancePort:     7401,
+		AssetRegistryEndpoint: "https://assets.rddl.io/register_asset",
+		WatchmenPort:          7401,
+		TokenDenom:            "plmnt",
+		StakeDenom:            "plmntstake",
+		FeeDenom:              "plmnt",
+		ConfigRootDir:         filepath.Join(currentUser.HomeDir, ".planetmint-go"),
+		PoPEpochs:             24, // 24 CometBFT epochs of 5s equate 120s
+		IssuanceEndpoint:      "lab.r3c.network",
+		IssuancePort:          7401,
 	}
 }
 
