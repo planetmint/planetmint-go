8f761992140c07e25a10ceb80b9db6827721bd76
Jürgen Eckel <juergen@riddleandcode.com>
2023-10-05 17:31:41 +0200

    added MsgReissueRDDLResult handling
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- x/dao/keeper/msg_server_reissue_rddl_result.go
+++ x/dao/keeper/msg_server_reissue_rddl_result.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	"context"
 
+	errorsmod "cosmossdk.io/errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/planetmint/planetmint-go/x/dao/types"
 )
@@ -10,8 +11,21 @@ import (
 func (k msgServer) ReissueRDDLResult(goCtx context.Context, msg *types.MsgReissueRDDLResult) (*types.MsgReissueRDDLResultResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	reissuance, found := k.GetReissuance(ctx, msg.GetBlockHeight())
+	if found != true {
+		return nil, errorsmod.Wrapf(types.ErrReissuanceNotFound, " for provided block height %u", msg.GetBlockHeight())
+	}
+	if reissuance.GetBlockHeight() != msg.GetBlockHeight() {
+		return nil, errorsmod.Wrapf(types.ErrWrongBlockHeight, " for provided block height %u", msg.GetBlockHeight())
+	}
+	if reissuance.GetProposer() != msg.GetProposer() {
+		return nil, errorsmod.Wrapf(types.ErrInvalidProposer, " for provided block height %u", msg.GetBlockHeight())
+	}
+	if reissuance.GetTxId() != "" {
+		return nil, errorsmod.Wrapf(types.ErrTXAlreadySet, " for provided block height %u", msg.GetBlockHeight())
+	}
+	reissuance.TxId = msg.GetTxId()
+	k.StoreReissuance(ctx, reissuance)
 
 	return &types.MsgReissueRDDLResultResponse{}, nil
 }
