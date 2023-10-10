88dee35aaa60603845c57d3a02b8c8f6673ff29e
Jürgen Eckel <juergen@riddleandcode.com>
2023-10-10 12:43:03 +0200

    from float to satoshi
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- x/dao/util.go
+++ x/dao/util.go
@@ -1,10 +1,10 @@
 package dao
 
 func GetReissuanceCommand(asset_id string, BlockHeight int64) string {
-	return "reissueasset " + asset_id + " 998.69"
+	return "reissueasset " + asset_id + " 99869000000"
 }
 
 func IsValidReissuanceCommand(reissuance_str string, asset_id string, BlockHeight uint64) bool {
-	expected := "reissueasset " + asset_id + " 998.69"
+	expected := "reissueasset " + asset_id + " 99869000000"
 	return reissuance_str == expected
 }
