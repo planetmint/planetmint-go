1e1138d0268a9896a1bd058e9b65b808eb20666e
Julian Strobl <jmastr@mailbox.org>
2023-11-13 12:42:52 +0100

    Switch to `revive` and fix findings (#171)
    * [ci] Switch from `nosnakecase` to `revive`
    
    * [revive] Fix findings
    
    Signed-off-by: Julian Strobl <jmastr@mailbox.org>

--- x/dao/abci.go
+++ x/dao/abci.go
@@ -11,7 +11,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-func BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock, k keeper.Keeper) {
+func BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock, _ keeper.Keeper) {
 	logger := ctx.Logger()
 	proposerAddress := req.Header.GetProposerAddress()
 
@@ -36,6 +36,6 @@ func isPoPHeight(height int64) bool {
 	return height%int64(cfg.PoPEpochs) == 0
 }
 
-func EndBlocker(ctx sdk.Context, req abci.RequestEndBlock, k keeper.Keeper) {
+func EndBlocker(ctx sdk.Context, _ abci.RequestEndBlock, k keeper.Keeper) {
 	k.DistributeCollectedFees(ctx)
 }
