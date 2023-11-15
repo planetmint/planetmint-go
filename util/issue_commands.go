c2589e0db80093daf45dee6d5b3a179d039dabf3
Julian Strobl <jmastr@mailbox.org>
2023-11-15 09:32:22 +0100

    Use RPC instead of `exec.Command(...)`
    // Closes #152
    
    Signed-off-by: Julian Strobl <jmastr@mailbox.org>

--- util/issue_commands.go
+++ util/issue_commands.go
@@ -1,58 +1,63 @@
 package util
 
 import (
-	"os/exec"
 	"strconv"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/planetmint/planetmint-go/config"
-	"github.com/planetmint/planetmint-go/x/dao/types"
+	"github.com/planetmint/planetmint-go/lib"
+	daotypes "github.com/planetmint/planetmint-go/x/dao/types"
 )
 
-func InitRDDLReissuanceProcess(ctx sdk.Context, proposerAddress string, txUnsigned string, blockHeight int64) error {
+func buildSignBroadcastTx(ctx sdk.Context, sendingValidatorAddress string, msg sdk.Msg) (err error) {
+	logger := ctx.Logger()
+	addr := sdk.MustAccAddressFromBech32(sendingValidatorAddress)
+	txBytes, txJSON, err := lib.BuildAndSignTx(addr, msg)
+	if err != nil {
+		return
+	}
+	logger.Debug("REISSUE: tx: " + txJSON)
+	_, err = lib.BroadcastTx(txBytes)
+	return
+}
+
+func InitRDDLReissuanceProcess(ctx sdk.Context, proposerAddress string, txUnsigned string, blockHeight int64) (err error) {
 	//get_last_PoPBlockHeight() // TODO: to be read form the upcoming PoP-store
 	logger := ctx.Logger()
-	// Construct the command
 	sendingValidatorAddress := config.GetConfig().ValidatorAddress
-	logger.Debug("REISSUE: create Proposal")
-	cmd := exec.Command("planetmint-god", "tx", "dao", "reissue-rddl-proposal",
-		"--from", sendingValidatorAddress, "-y",
-		proposerAddress, txUnsigned, strconv.FormatInt(blockHeight, 10))
-
-	logger.Debug("REISSUE: create Proposal")
-	return cmd.Start()
+	logger.Debug("REISSUE: create Reissuance Proposal")
+	msg := daotypes.NewMsgReissueRDDLProposal(sendingValidatorAddress, proposerAddress, txUnsigned, blockHeight)
+	err = buildSignBroadcastTx(ctx, sendingValidatorAddress, msg)
+	return
 }
 
-func SendRDDLReissuanceResult(ctx sdk.Context, proposerAddress string, txID string, blockHeight int64) error {
+func SendRDDLReissuanceResult(ctx sdk.Context, proposerAddress string, txID string, blockHeight int64) (err error) {
 	logger := ctx.Logger()
-	// Construct the command
 	sendingValidatorAddress := config.GetConfig().ValidatorAddress
-	logger.Debug("REISSUE: create Result")
-	cmd := exec.Command("planetmint-god", "tx", "dao", "reissue-rddl-result",
-		"--from", sendingValidatorAddress, "-y",
-		proposerAddress, txID, strconv.FormatInt(blockHeight, 10))
-	logger.Debug("REISSUE: create Result")
-	return cmd.Start()
+	logger.Debug("REISSUE: create Reissuance Result")
+	msg := daotypes.NewMsgReissueRDDLResult(sendingValidatorAddress, proposerAddress, txID, blockHeight)
+	err = buildSignBroadcastTx(ctx, sendingValidatorAddress, msg)
+	return
 }
 
-func SendRDDLDistributionRequest(ctx sdk.Context, distribution types.DistributionOrder) error {
+func SendRDDLDistributionRequest(ctx sdk.Context, distribution daotypes.DistributionOrder) (err error) {
 	logger := ctx.Logger()
-	// Construct the command
 	sendingValidatorAddress := config.GetConfig().ValidatorAddress
-	cmd := exec.Command("planetmint-god", "tx", "dao", "distribution-request",
-		"--from", sendingValidatorAddress, "-y", "'"+distribution.String()+"'")
-	logger.Debug("REISSUE: create Result")
-	return cmd.Start()
+	logger.Debug("REISSUE: create Distribution Request")
+	msg := daotypes.NewMsgDistributionRequest(sendingValidatorAddress, &distribution)
+	err = buildSignBroadcastTx(ctx, sendingValidatorAddress, msg)
+	return
 }
 
-func SendRDDLDistributionResult(ctx sdk.Context, lastPoP string, daoTxid string, invTxid string, popTxid string) error {
+func SendRDDLDistributionResult(ctx sdk.Context, lastPoP string, daoTxID string, invTxID string, popTxID string) (err error) {
 	logger := ctx.Logger()
-	// Construct the command
 	sendingValidatorAddress := config.GetConfig().ValidatorAddress
-	logger.Debug("REISSUE: create Result")
-	cmd := exec.Command("planetmint-god", "tx", "dao", "distribution-result",
-		"--from", sendingValidatorAddress, "-y",
-		lastPoP, daoTxid, invTxid, popTxid)
-	logger.Debug("REISSUE: create Result")
-	return cmd.Start()
+	logger.Debug("REISSUE: create Distribution Result")
+	iLastPoP, err := strconv.ParseInt(lastPoP, 10, 64)
+	if err != nil {
+		return
+	}
+	msg := daotypes.NewMsgDistributionResult(sendingValidatorAddress, iLastPoP, daoTxID, invTxID, popTxID)
+	err = buildSignBroadcastTx(ctx, sendingValidatorAddress, msg)
+	return
 }
