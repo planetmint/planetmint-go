fd58d4429ab5c04ed93f897c25b190ce63b7d259
Jürgen Eckel <juergen@riddleandcode.com>
2023-11-21 12:43:05 +0100

    added reissuance details, the concept of popepochs and popcycles
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- x/dao/keeper/reissuance_test.go
+++ x/dao/keeper/reissuance_test.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/planetmint/planetmint-go/config"
 	keepertest "github.com/planetmint/planetmint-go/testutil/keeper"
 	"github.com/stretchr/testify/assert"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/planetmint/planetmint-go/x/dao/keeper"
+	DaoKeeper "github.com/planetmint/planetmint-go/x/dao/keeper"
 	"github.com/planetmint/planetmint-go/x/dao/types"
 )
 
@@ -33,4 +35,15 @@ func TestGetReissuances(t *testing.T) {
 		assert.True(t, found)
 		assert.Equal(t, item, reissuance)
 	}
+}
+
+func TestReissuanceValueComputation(t *testing.T) {
+	popsPerEpoch := float64(config.GetConfig().PoPEpochs)
+	assert.Equal(t, "998.69000000", DaoKeeper.GetReissuanceAsStringValue(1))
+	assert.Equal(t, "499.34000000", DaoKeeper.GetReissuanceAsStringValue(int64(DaoKeeper.PoPsPerCycle*popsPerEpoch*1+1)))
+	assert.Equal(t, "249.67000000", DaoKeeper.GetReissuanceAsStringValue(int64(DaoKeeper.PoPsPerCycle*popsPerEpoch*2+1)))
+	assert.Equal(t, "124.83000000", DaoKeeper.GetReissuanceAsStringValue(int64(DaoKeeper.PoPsPerCycle*popsPerEpoch*3+1)))
+	assert.Equal(t, "62.42000000", DaoKeeper.GetReissuanceAsStringValue(int64(DaoKeeper.PoPsPerCycle*popsPerEpoch*4+1)))
+	assert.Equal(t, "0.0", DaoKeeper.GetReissuanceAsStringValue(int64(DaoKeeper.PoPsPerCycle*popsPerEpoch*5+1)))
+
 }
