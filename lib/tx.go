14f8749125ee27357c5cc4601bc713f3df348a9e
Julian Strobl <jmastr@mailbox.org>
2023-11-27 16:11:07 +0100

    Synchronize sequence number across services and for offline mode (#199)
    * [lib] Refactor `getClientContextAndTxFactory()`
    So that we can pass the account number and sequence number from
    outside.
    
    * [lib] Add `BroadcastTxWithFileLock()`
    
    This implements "offline mode" via RPC, but instead of keeping track of
    the sequence numbers, we write them into a file and lock them. This way, we can synchronize multiple processes using the same address, trying to
    send transactions simultaneously.
    
    // Closes https://github.com/rddl-network/issues/issues/46
    
    Signed-off-by: Julian Strobl <jmastr@mailbox.org>
    
    * [lib] Introduce variable for error
    * [lib] Improve error handling in `broadcastTx()`
    * Use `BroadcastTxWithFileLock()`
    // See https://github.com/rddl-network/issues/issues/46
    
    ---------
    
    Signed-off-by: Julian Strobl <jmastr@mailbox.org>

--- lib/tx.go
+++ lib/tx.go
@@ -1,8 +1,10 @@
 package lib
 
 import (
+	"bufio"
 	"bytes"
 	"context"
+	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -10,7 +12,11 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/user"
+	"path/filepath"
 	"strconv"
+	"strings"
+	"syscall"
 
 	comethttp "github.com/cometbft/cometbft/rpc/client/http"
 	"github.com/cosmos/cosmos-sdk/client"
@@ -18,6 +24,8 @@ import (
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
+
+var ErrTypeAssertionFailed = errors.New("type assertion failed")
 
 // Result defines a generic way to receive responses from the RPC endpoint.
 type Result struct {
@@ -63,6 +71,39 @@ func getAccountNumberAndSequence(goCtx context.Context, address sdk.AccAddress) 
 }
 
 func getClientContextAndTxFactory(goCtx context.Context, address sdk.AccAddress) (clientCtx client.Context, txf tx.Factory, err error) {
+	clientCtx, err = getClientContext(address)
+	if err != nil {
+		return
+	}
+	accountNumber, sequence, err := getAccountNumberAndSequence(goCtx, clientCtx.FromAddress)
+	if err != nil {
+		return
+	}
+	txf = getTxFactoryWithAccountNumberAndSequence(clientCtx, accountNumber, sequence)
+	return
+}
+
+func getClientContextAndTxFactoryWithAccountNumberAndSequence(address sdk.AccAddress, accountNumber, sequence uint64) (clientCtx client.Context, txf tx.Factory, err error) {
+	clientCtx, err = getClientContext(address)
+	if err != nil {
+		return
+	}
+	txf = getTxFactoryWithAccountNumberAndSequence(clientCtx, accountNumber, sequence)
+	return
+}
+
+func getTxFactoryWithAccountNumberAndSequence(clientCtx client.Context, accountNumber, sequence uint64) (txf tx.Factory) {
+	return tx.Factory{}.
+		WithAccountNumber(accountNumber).
+		WithChainID(clientCtx.ChainID).
+		WithGas(200000).
+		WithGasPrices("0.000005plmnt").
+		WithKeybase(clientCtx.Keyring).
+		WithSequence(sequence).
+		WithTxConfig(clientCtx.TxConfig)
+}
+
+func getClientContext(address sdk.AccAddress) (clientCtx client.Context, err error) {
 	encodingConfig := GetConfig().EncodingConfig
 
 	rootDir := GetConfig().RootDir
@@ -108,20 +149,6 @@ func getClientContextAndTxFactory(goCtx context.Context, address sdk.AccAddress)
 		TxConfig:       encodingConfig.TxConfig,
 	}
 
-	accountNumber, sequence, err := getAccountNumberAndSequence(goCtx, address)
-	if err != nil {
-		return
-	}
-
-	txf = tx.Factory{}.
-		WithAccountNumber(accountNumber).
-		WithChainID(clientCtx.ChainID).
-		WithGas(200000).
-		WithGasPrices("0.000005plmnt").
-		WithKeybase(clientCtx.Keyring).
-		WithSequence(sequence).
-		WithTxConfig(clientCtx.TxConfig)
-
 	return
 }
 
@@ -151,12 +178,135 @@ func BroadcastTx(goCtx context.Context, address sdk.AccAddress, msgs ...sdk.Msg)
 	if err != nil {
 		return
 	}
-	err = tx.GenerateOrBroadcastTxWithFactory(clientCtx, txf, msgs...)
-	output, ok := clientCtx.Output.(*bytes.Buffer)
-	if !ok {
-		err = errors.New("type assertion failed")
+	broadcastTxResponseJSON, err = broadcastTx(clientCtx, txf, msgs...)
+	return
+}
+
+func broadcastTxWithAccountNumberAndSequence(address sdk.AccAddress, accountNumber, sequence uint64, msgs ...sdk.Msg) (broadcastTxResponseJSON string, err error) {
+	clientCtx, txf, err := getClientContextAndTxFactoryWithAccountNumberAndSequence(address, accountNumber, sequence)
+	if err != nil {
 		return
 	}
+	broadcastTxResponseJSON, err = broadcastTx(clientCtx, txf, msgs...)
+	return
+}
+
+func broadcastTx(clientCtx client.Context, txf tx.Factory, msgs ...sdk.Msg) (broadcastTxResponseJSON string, err error) {
+	err = tx.GenerateOrBroadcastTxWithFactory(clientCtx, txf, msgs...)
+	if err != nil {
+		return
+	}
+	output, ok := clientCtx.Output.(*bytes.Buffer)
+	if !ok {
+		err = ErrTypeAssertionFailed
+		return
+	}
+
+	result := make(map[string]interface{})
+	err = json.Unmarshal(output.Bytes(), &result)
+	if err != nil {
+		return
+	}
+	code, ok := result["code"].(float64)
+	if !ok {
+		err = ErrTypeAssertionFailed
+		return
+	}
+	if code != 0 {
+		err = errors.New(output.String())
+		return
+	}
+
 	broadcastTxResponseJSON = output.String()
+	return
+}
+
+// BroadcastTxWithFileLock broadcasts a transaction via gRPC and synchronises requests via a file lock.
+func BroadcastTxWithFileLock(goCtx context.Context, address sdk.AccAddress, msgs ...sdk.Msg) (broadcastTxResponseJSON string, err error) {
+	usr, err := user.Current()
+	if err != nil {
+		return
+	}
+	homeDir := usr.HomeDir
+
+	addrHex := hex.EncodeToString(address)
+	filename := filepath.Join(GetConfig().RootDir, addrHex+".sequence")
+
+	// Expand tilde to user's home directory.
+	if filename == "~" {
+		filename = homeDir
+	} else if strings.HasPrefix(filename, "~/") {
+		filename = filepath.Join(homeDir, filename[2:])
+	}
+
+	file, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	// Get file lock.
+	err = syscall.Flock(int(file.Fd()), syscall.LOCK_EX)
+	if err != nil {
+		return
+	}
+	defer func() {
+		if err := syscall.Flock(int(file.Fd()), syscall.LOCK_UN); err != nil {
+			return
+		}
+	}()
+
+	var sequenceString string
+	lineCount := int64(0)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		sequenceString = scanner.Text()
+		lineCount++
+	}
+	err = scanner.Err()
+	if err != nil {
+		return
+	}
+
+	// Get sequence number from chain.
+	accountNumber, sequence, err := getAccountNumberAndSequence(goCtx, address)
+	if err != nil {
+		return
+	}
+
+	if lineCount == 0 {
+		// File does not exist yet.
+		sequenceString = strconv.FormatUint(sequence, 10)
+	} else if lineCount != 1 {
+		err = errors.New("Malformed " + filename + ": wrong number of lines")
+		return
+	}
+
+	sequenceCount, err := strconv.ParseUint(sequenceString, 10, 64)
+	if err != nil {
+		return
+	}
+
+	// Sequence number on chain is bigger than in text file.
+	// Someone manually sent a transaction from our account?
+	if sequence > sequenceCount {
+		sequenceCount = sequence
+	}
+
+	broadcastTxResponseJSON, err = broadcastTxWithAccountNumberAndSequence(address, accountNumber, sequenceCount, msgs...)
+	if err != nil {
+		return
+	}
+
+	// Increase counter for next round.
+	sequenceCount++
+
+	_, err = file.Seek(0, io.SeekStart)
+	if err != nil {
+		return
+	}
+
+	_, err = file.WriteString(strconv.FormatUint(sequenceCount, 10) + "\n")
+
 	return
 }
