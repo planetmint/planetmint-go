ede70b073bf0fd1c95daef32ea3767dec32ac234
Julian Strobl <jmastr@mailbox.org>
2023-12-11 15:53:58 +0100

    Replace call to external python program (#215)
    * Split asset registry endpoint
    
    Into scheme, domain and path. We need the domain in the liquid contract.
    
    * Switch to pure Go implementation
    
    This is a reimplementation of the `issue2liquid.py`.
    
    See https://github.com/rddl-network/issuer_service
    
    // Closes #196
    
    * Move elements RPC URL into config
    
    Signed-off-by: Julian Strobl <jmastr@mailbox.org>

--- x/machine/types/types.go
+++ x/machine/types/types.go
@@ -1,1 +1,14 @@
 package types
+
+type Entity struct {
+	Domain string `json:"domain"`
+}
+
+type Contract struct {
+	Entity       Entity `json:"entity"`
+	IssuerPubkey string `json:"issuer_pubkey"` //nolint:tagliatelle // the format liquid network needs it
+	MachineAddr  string `json:"machine_addr"`  //nolint:tagliatelle // the format liquid network needs it
+	Name         string `json:"name"`
+	Precision    uint64 `json:"precision"`
+	Version      uint64 `json:"version"`
+}
