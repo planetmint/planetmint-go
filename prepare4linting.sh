363d82d3447595baad3c5d4e0d8a2553db8d72d2
Julian Strobl <jmastr@mailbox.org>
2023-12-11 11:41:45 +0100

    [ci] Improve staticcheck setup (#221)
    * [staticcheck] Fix comments on exported functions and structs
    
    * [ci] Improve staticcheck setup
    
    - Add global staticcheck.conf file
    - Ignore: at least one file in a package should have a package comment (ST1000)
    - Ignore deprecation warnings. They are reported by golangci-lint and
      there we have a proper way to exclude generated files.
    
    Signed-off-by: Julian Strobl <jmastr@mailbox.org>

--- prepare4linting.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export LINT="//lint:file-ignore SA1019 Ignore all deprecation errors, it's generated"
-
-# Add lint-ignore comment to beginning of files                                
-sed -i "1i${LINT}" ./x/asset/types/query.pb.gw.go
-sed -i "1i${LINT}" ./x/machine/types/query.pb.gw.go
-sed -i "1i${LINT}" ./x/dao/types/query.pb.gw.go
