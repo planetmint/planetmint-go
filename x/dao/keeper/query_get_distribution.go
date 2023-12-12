73b0d1b4c8edb47ac7c319345553860e455c52bf
Jürgen Eckel <eckelj@users.noreply.github.com>
2023-12-12 10:13:27 +0100

    Add GetDistribution query (#227)
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

---
+++ x/dao/keeper/query_get_distribution.go
@@ -0,0 +1,25 @@
+package keeper
+
+import (
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/planetmint/planetmint-go/x/dao/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (k Keeper) GetDistribution(goCtx context.Context, req *types.QueryGetDistributionRequest) (*types.QueryGetDistributionResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	distribution, found := k.LookupDistributionOrder(ctx, req.GetLastPopHeight())
+	if !found {
+		return nil, status.Error(codes.NotFound, "distribution not found")
+	}
+
+	return &types.QueryGetDistributionResponse{Distribution: &distribution}, nil
+}
