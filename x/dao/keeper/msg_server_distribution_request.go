3058ca6b4db07e7e33b535cde8cf1d7f3af40e8d
Jürgen Eckel <juergen@riddleandcode.com>
2023-12-12 11:06:01 +0100

    removed go routine
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- x/dao/keeper/msg_server_distribution_request.go
+++ x/dao/keeper/msg_server_distribution_request.go
@@ -14,26 +14,24 @@ func (k msgServer) DistributionRequest(goCtx context.Context, msg *types.MsgDist
 	validatorIdentity, validResult := util.GetValidatorCometBFTIdentity(ctx)
 	if validResult && msg.Distribution.GetProposer() == validatorIdentity {
 		util.GetAppLogger().Info(ctx, "distribution request: Entering Asset Distribution Mode")
-		go func() {
-			// issue three distributions:
-			investorTx, err := util.DistributeAsset(msg.Distribution.InvestorAddr, msg.Distribution.InvestorAmount)
-			if err != nil {
-				util.GetAppLogger().Error(ctx, "Distribution Request: could not distribute asset to Investors: ", err.Error())
-			}
-			popTx, err := util.DistributeAsset(msg.Distribution.PopAddr, msg.Distribution.PopAmount)
-			if err != nil {
-				util.GetAppLogger().Error(ctx, "Distribution Request: could not distribute asset to PoP: ", err.Error())
-			}
-			daoTx, err := util.DistributeAsset(msg.Distribution.DaoAddr, msg.Distribution.DaoAmount)
-			if err != nil {
-				util.GetAppLogger().Error(ctx, "Distribution Request: could not distribute asset to DAO: ", err.Error())
-			}
+		// issue three distributions:
+		investorTx, err := util.DistributeAsset(msg.Distribution.InvestorAddr, msg.Distribution.InvestorAmount)
+		if err != nil {
+			util.GetAppLogger().Error(ctx, "Distribution Request: could not distribute asset to Investors: ", err.Error())
+		}
+		popTx, err := util.DistributeAsset(msg.Distribution.PopAddr, msg.Distribution.PopAmount)
+		if err != nil {
+			util.GetAppLogger().Error(ctx, "Distribution Request: could not distribute asset to PoP: ", err.Error())
+		}
+		daoTx, err := util.DistributeAsset(msg.Distribution.DaoAddr, msg.Distribution.DaoAmount)
+		if err != nil {
+			util.GetAppLogger().Error(ctx, "Distribution Request: could not distribute asset to DAO: ", err.Error())
+		}
 
-			msg.Distribution.InvestorTxID = investorTx
-			msg.Distribution.PopTxID = popTx
-			msg.Distribution.DaoTxID = daoTx
-			util.SendDistributionResult(goCtx, msg.Distribution.LastPop, daoTx, investorTx, popTx)
-		}()
+		msg.Distribution.InvestorTxID = investorTx
+		msg.Distribution.PopTxID = popTx
+		msg.Distribution.DaoTxID = daoTx
+		util.SendDistributionResult(goCtx, msg.Distribution.LastPop, daoTx, investorTx, popTx)
 	}
 	util.GetAppLogger().Info(ctx, "distribution request: storing distribution")
 	k.StoreDistributionOrder(ctx, *msg.GetDistribution())
