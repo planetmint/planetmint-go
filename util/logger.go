9371e724f67b31480bac38c25fa9d53f100b4bfd
Julian Strobl <jmastr@mailbox.org>
2023-12-12 13:16:22 +0100

    [tests] Enable AppLogger during testing
    Signed-off-by: Julian Strobl <jmastr@mailbox.org>

--- util/logger.go
+++ util/logger.go
@@ -1,8 +1,14 @@
 package util
 
-import sdk "github.com/cosmos/cosmos-sdk/types"
+import (
+	"sync"
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
 
 type AppLogger struct {
+	testingLogger *testing.T
 }
 
 var (
@@ -19,19 +25,35 @@ func init() {
 func GetAppLogger() *AppLogger {
 	initAppLogger.Do(func() {
 		appLogger = &AppLogger{
+			testingLogger: nil,
 		}
 	})
 	return appLogger
 }
 
+func (logger *AppLogger) SetTestingLogger(testingLogger *testing.T) *AppLogger {
+	logger.testingLogger = testingLogger
+	return logger
+}
+
+func (logger *AppLogger) testingLog(msg string, keyvals ...interface{}) {
+	if logger.testingLogger == nil {
+		return
+	}
+	logger.testingLogger.Log(msg, keyvals)
+}
+
 func (logger *AppLogger) Info(ctx sdk.Context, msg string, keyvals ...interface{}) {
+	logger.testingLog(globalApplicationLoggerTag+msg, keyvals...)
 	ctx.Logger().Info(globalApplicationLoggerTag+msg, keyvals...)
 }
 
 func (logger *AppLogger) Debug(ctx sdk.Context, msg string, keyvals ...interface{}) {
+	logger.testingLog(globalApplicationLoggerTag+msg, keyvals...)
 	ctx.Logger().Debug(globalApplicationLoggerTag+msg, keyvals...)
 }
 
 func (logger *AppLogger) Error(ctx sdk.Context, msg string, keyvals ...interface{}) {
+	logger.testingLog(globalApplicationLoggerTag+msg, keyvals...)
 	ctx.Logger().Error(globalApplicationLoggerTag+msg, keyvals...)
 }
