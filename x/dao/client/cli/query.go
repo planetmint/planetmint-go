022a02f8943d53318516640be533763d99379a0a
Lorenz Herzberger <lorenzherzberger@gmail.com>
2023-12-13 10:07:47 +0100

    add challenges query
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

--- x/dao/client/cli/query.go
+++ x/dao/client/cli/query.go
@@ -32,6 +32,8 @@ func GetQueryCmd(_ string) *cobra.Command {
 
 	cmd.AddCommand(CmdGetChallenge())
 
+	cmd.AddCommand(CmdChallenges())
+
 	// this line is used by starport scaffolding # 1
 
 	return cmd
---
+++ x/dao/keeper/query_challenges.go
@@ -0,0 +1,36 @@
+package keeper
+
+import (
+	"context"
+
+	"github.com/cosmos/cosmos-sdk/store/prefix"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
+	"github.com/planetmint/planetmint-go/x/dao/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (k Keeper) Challenges(goCtx context.Context, req *types.QueryChallengesRequest) (*types.QueryChallengesResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	challenges := make([]types.Challenge, 0)
+
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.ChallengeKey))
+	pageRes, err := query.Paginate(store, req.Pagination, func(_ []byte, value []byte) (err error) {
+		var challenge types.Challenge
+		err = challenge.Unmarshal(value)
+		challenges = append(challenges, challenge)
+		return
+	})
+
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "paginate: %v", err)
+	}
+
+	return &types.QueryChallengesResponse{Challenges: challenges, Pagination: pageRes}, nil
+}
