b2d7081e6565a842fdca1ffb748d7b8eb258889f
Lorenz Herzberger <lorenzherzberger@gmail.com>
2024-01-03 15:00:32 +0100

    refactor: split up asset e2e test suites
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

--- tests/e2e/asset/rest.go
+++ tests/e2e/asset/rest.go
@@ -3,16 +3,61 @@ package asset
 import (
 	"fmt"
 
+	"github.com/planetmint/planetmint-go/config"
+	"github.com/planetmint/planetmint-go/lib"
 	"github.com/planetmint/planetmint-go/testutil"
+	e2etestutil "github.com/planetmint/planetmint-go/testutil/e2e"
+	"github.com/planetmint/planetmint-go/testutil/network"
 	"github.com/planetmint/planetmint-go/testutil/sample"
+	"github.com/stretchr/testify/suite"
 
 	assettypes "github.com/planetmint/planetmint-go/x/asset/types"
 
 	txtypes "github.com/cosmos/cosmos-sdk/types/tx"
 )
 
+type RestE2ETestSuite struct {
+	suite.Suite
+
+	cfg     network.Config
+	network *network.Network
+}
+
+func NewRestE2ETestSuite(cfg network.Config) *RestE2ETestSuite {
+	return &RestE2ETestSuite{cfg: cfg}
+}
+
+// SetupSuite initializes asset E2ETestSuite
+func (s *RestE2ETestSuite) SetupSuite() {
+	conf := config.GetConfig()
+	conf.FeeDenom = "stake"
+
+	s.T().Log("setting up e2e test suite")
+
+	s.network = network.New(s.T())
+	err := e2etestutil.AttestMachine(s.network, sample.Name, sample.Mnemonic, 0)
+	s.Require().NoError(err)
+
+	val := s.network.Validators[0]
+	k, err := val.ClientCtx.Keyring.Key(sample.Name)
+	s.Require().NoError(err)
+
+	addr, _ := k.GetAddress()
+
+	clientCtx := val.ClientCtx.
+		WithFromAddress(addr).
+		WithFromName(sample.Name)
+	libConfig := lib.GetConfig()
+	libConfig.SetClientCtx(clientCtx)
+}
+
+// TearDownSuite clean up after testing
+func (s *RestE2ETestSuite) TearDownSuite() {
+	s.T().Log("tearing down e2e test suite")
+}
+
 // TestNotarizeAssetREST notarizes asset over REST endpoint
-func (s *E2ETestSuite) TestNotarizeAssetREST() {
+func (s *RestE2ETestSuite) TestNotarizeAssetREST() {
 	val := s.network.Validators[0]
 
 	// Create Msg
