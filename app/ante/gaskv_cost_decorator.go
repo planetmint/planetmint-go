b4547e841a75c23eec56c3fec2d97612279a5a83
Julian Strobl <jmastr@mailbox.org>
2024-01-11 09:41:59 +0100

    refactor: return early to reduce indentation
    Signed-off-by: Julian Strobl <jmastr@mailbox.org>

--- app/ante/gaskv_cost_decorator.go
+++ app/ante/gaskv_cost_decorator.go
@@ -13,26 +13,30 @@ func NewGasKVCostDecorator(sk StakingKeeper) GasKVCostDecorator {
 }
 
 func (gc GasKVCostDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (_ sdk.Context, err error) {
-	if !simulate && ctx.BlockHeight() != 0 {
-		msgs := tx.GetMsgs()
-		signers := msgs[0].GetSigners()
-		signer := signers[0]
-
-		valAddr := sdk.ValAddress(signer)
-		_, found := gc.sk.GetValidator(ctx, valAddr)
-
-		if found {
-			ctx = ctx.WithKVGasConfig(sdk.GasConfig{
-				HasCost:          0,
-				DeleteCost:       0,
-				ReadCostFlat:     0,
-				ReadCostPerByte:  0,
-				WriteCostFlat:    0,
-				WriteCostPerByte: 0,
-				IterNextCostFlat: 0,
-			})
-		}
+	if simulate || ctx.BlockHeight() == 0 {
+		return next(ctx, tx, simulate)
 	}
+
+	msgs := tx.GetMsgs()
+	signers := msgs[0].GetSigners()
+	signer := signers[0]
+
+	valAddr := sdk.ValAddress(signer)
+	_, found := gc.sk.GetValidator(ctx, valAddr)
+
+	if !found {
+		return next(ctx, tx, simulate)
+	}
+
+	ctx = ctx.WithKVGasConfig(sdk.GasConfig{
+		HasCost:          0,
+		DeleteCost:       0,
+		ReadCostFlat:     0,
+		ReadCostPerByte:  0,
+		WriteCostFlat:    0,
+		WriteCostPerByte: 0,
+		IterNextCostFlat: 0,
+	})
 
 	return next(ctx, tx, simulate)
 }
