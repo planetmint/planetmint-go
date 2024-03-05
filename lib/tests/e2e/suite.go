af0659da2feb2f0bf0bf4f6691e0167ec30a698e
Jürgen Eckel <juergen@riddleandcode.com>
2024-03-05 15:37:55 +0100

    removed termination wait group from lib/test/e2e/suite
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- lib/tests/e2e/suite.go
+++ lib/tests/e2e/suite.go
@@ -9,7 +9,6 @@ import (
 	clitestutil "github.com/planetmint/planetmint-go/testutil/cli"
 	"github.com/planetmint/planetmint-go/testutil/network"
 	"github.com/planetmint/planetmint-go/testutil/sample"
-	"github.com/planetmint/planetmint-go/util"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
@@ -37,7 +36,6 @@ func (s *E2ETestSuite) SetupSuite() {
 
 // TearDownSuite clean up after testing
 func (s *E2ETestSuite) TearDownSuite() {
-	util.TerminationWaitGroup.Wait()
 	s.T().Log("tearing down e2e test suite")
 }
 
