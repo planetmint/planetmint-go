9dc7acdf6c0f331cf10cb124411c626e15ec925e
Julian Strobl <jmastr@mailbox.org>
2024-03-06 11:38:09 +0100

    refactor: generate contract from struct in test
    - also use default params
    
    Signed-off-by: Julian Strobl <jmastr@mailbox.org>

--- util/machine_nft_test.go
+++ util/machine_nft_test.go
@@ -2,6 +2,7 @@ package util_test
 
 import (
 	"context"
+	"encoding/json"
 	"testing"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -10,19 +11,35 @@ import (
 	"github.com/planetmint/planetmint-go/testutil/sample"
 	"github.com/planetmint/planetmint-go/util"
 	"github.com/planetmint/planetmint-go/util/mocks"
+	"github.com/planetmint/planetmint-go/x/machine/types"
 	elements "github.com/rddl-network/elements-rpc"
 	elementsmocks "github.com/rddl-network/elements-rpc/utils/mocks"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRegisterNFT(t *testing.T) {
-	url := "https://testnet-assets.rddl.io/register_asset"
-	contract := `{"entity":{"domain":"testnet-assets.rddl.io"},"issuer_pubkey":"020000000000000000000000000000000000000000000000000000000000000000","machine_addr":"plmnt10mq5nj8jhh27z7ejnz2ql3nh0qhzjnfvy50877","name":"machine","precision":0,"version":0}`
+	params := types.DefaultParams()
+	url := params.AssetRegistryScheme + "://" + params.AssetRegistryDomain + "/" + params.AssetRegistryPath
+	entity := types.Entity{
+		Domain: params.AssetRegistryDomain,
+	}
+	c := types.Contract{
+		Entity:       entity,
+		IssuerPubkey: "020000000000000000000000000000000000000000000000000000000000000000",
+		MachineAddr:  "plmnt10mq5nj8jhh27z7ejnz2ql3nh0qhzjnfvy50877",
+		Name:         "machine",
+		Precision:    0,
+		Version:      0,
+	}
+	contractBytes, err := json.Marshal(c)
+	assert.NoError(t, err)
+
+	contract := string(contractBytes)
 	asset := "0000000000000000000000000000000000000000000000000000000000000000"
 	goctx := context.Background()
 
 	util.RegisterAssetServiceHTTPClient = &mocks.MockClient{}
-	err := util.RegisterAsset(goctx, asset, contract, url)
+	err = util.RegisterAsset(goctx, asset, contract, url)
 	assert.NoError(t, err)
 }
 
