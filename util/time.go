f68d0df98b131ed98e2cd408e2d7119597e78957
Jürgen Eckel <juergen@riddleandcode.com>
2024-05-15 11:12:59 +0200

    * avoid using app.Logger to have a consistent log from the beginning * made lazyLoading local to the module * fixed ticker bug so that the tasks are executed in an endless loop * fixed timezone bug of Add Participant. only validator local time is used not the timestamp from the mqtt messages as these might come from a different time zone (resulted in delayed deletion from the activeActors DB) * improved connection management for the monitor * added a watchdog task to restart the monitor in case of connection loss (every 2 min)
    Signed-off-by: Jürgen Eckel <juergen@riddleandcode.com>

--- util/time.go
+++ util/time.go
@@ -1,6 +1,8 @@
 package util
 
 import (
+	"log"
+	"strconv"
 	"time"
 )
 
@@ -8,7 +10,7 @@ func String2UnixTime(timeInput string) (int64, error) {
 	// Layout specifying the format of the input string
 	// Note: Go uses a specific reference time (Mon Jan 2 15:04:05 MST 2006) to define format layouts
 	layout := "2006-01-02T15:04:05"
-
+	log.Println("[app] [Monitor] [time] add time string: " + timeInput)
 	// Parse the string into a time.Time struct in local time zone
 	parsedTime, err := time.Parse(layout, timeInput)
 	if err != nil {
@@ -18,6 +20,6 @@ func String2UnixTime(timeInput string) (int64, error) {
 	// Convert to UTC if not already
 	utcTime := parsedTime.UTC()
 	unixTime := utcTime.Unix()
-
+	log.Println("[app] [Monitor] [time] unix time: " + strconv.FormatInt(unixTime, 10))
 	return unixTime, nil
 }
