f8933a99d87771383e798c4fcec04a59a90766a3
Lorenz Herzberger <lorenzherzberger@gmail.com>
2024-05-29 16:47:36 +0200

    chore: add migration for new store mechanics
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

---
+++ x/asset/migrations/v2/store.go
@@ -0,0 +1,39 @@
+package v2
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/store/prefix"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/planetmint/planetmint-go/util"
+	"github.com/planetmint/planetmint-go/x/asset/types"
+)
+
+func MigrateStore(ctx sdk.Context, storeKey storetypes.StoreKey, cdc codec.BinaryCodec) error {
+	store := prefix.NewStore(ctx.KVStore(storeKey), types.KeyPrefix(types.AssetKey))
+
+	mapping := make(map[string][][]byte)
+
+	// read all cids
+	iterator := store.Iterator(nil, nil)
+	defer iterator.Close()
+	for ; iterator.Valid(); iterator.Next() {
+		addressBytes := iterator.Value()
+		cidBytes := iterator.Key()
+
+		// map all cids by address
+		mapping[string(addressBytes)] = append(mapping[string(addressBytes)], cidBytes)
+	}
+
+	// store all cids with new key
+	for address, cids := range mapping {
+		assetByAddressStore := prefix.NewStore(ctx.KVStore(storeKey), types.AddressPrefix(address))
+		for i, cid := range cids {
+			assetByAddressStore.Set(util.SerializeUint64(uint64(i)), cid)
+		}
+		addressAssetCountStore := prefix.NewStore(ctx.KVStore(storeKey), types.KeyPrefix(types.AssetKey))
+		addressAssetCountStore.Set(types.AddressCountKey(address), util.SerializeUint64(uint64(len(cids))))
+	}
+
+	return nil
+}
