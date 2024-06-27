208ad70ab001ffd33327f779c09d8c14d20a1d0b
Lorenz Herzberger <lorenzherzberger@gmail.com>
2024-06-27 16:18:35 +0200

    feat: add validator pop reward as chain param
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

---
+++ x/dao/migrations/v3/params.go
@@ -0,0 +1,31 @@
+package v3
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/planetmint/planetmint-go/x/dao/types"
+)
+
+func MigrateParams(ctx sdk.Context, storeKey storetypes.StoreKey, cdc codec.BinaryCodec) error {
+	store := ctx.KVStore(storeKey)
+	paramBytes := store.Get(types.KeyPrefix(types.ParamsKey))
+
+	var params types.Params
+	err := cdc.Unmarshal(paramBytes, &params)
+	if err != nil {
+		return err
+	}
+
+	params.ValidatorPopReward = 1
+
+	// migratedParams := types.NewParams(params.MintAddress, params.TokenDenom, params.StagedDenom, params.ClaimDenom, params.ReissuanceAsset, params.ReissuanceEpochs, params.PopEpochs, params.DistributionOffset, params.DistributionAddressEarlyInv, params.DistributionAddressInvestor, params.DistributionAddressStrategic, params.DistributionAddressDao, params.DistributionAddressPop, params.MqttResponseTimeout, params.ClaimAddress, params.TxGasLimit, 1)
+	bz, err := cdc.Marshal(&params)
+	if err != nil {
+		return err
+	}
+
+	store.Set(types.KeyPrefix(types.ParamsKey), bz)
+
+	return nil
+}
