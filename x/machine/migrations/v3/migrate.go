700944ffa78e57f386f5a1dc3bacca2ecb6bd715
Lorenz Herzberger <lorenzherzberger@gmail.com>
2024-07-22 11:06:05 +0200

    fix: iterate over correct store in migration
    Signed-off-by: Lorenz Herzberger <lorenzherzberger@gmail.com>

--- x/machine/migrations/v3/migrate.go
+++ x/machine/migrations/v3/migrate.go
@@ -11,19 +11,21 @@ import (
 )
 
 func MigrateStore(ctx sdk.Context, storeKey storetypes.StoreKey, cdc codec.BinaryCodec) error {
-	store := prefix.NewStore(ctx.KVStore(storeKey), types.KeyPrefix(types.ActivatedTACounterPrefix))
+	store := prefix.NewStore(ctx.KVStore(storeKey), types.KeyPrefix(types.TrustAnchorKey))
 
 	count := uint64(0)
-
 	iterator := store.Iterator(nil, nil)
 	defer iterator.Close()
 	for ; iterator.Valid(); iterator.Next() {
-		count++
+		if iterator.Value()[0] == 1 {
+			count++
+		}
 	}
 
 	bz := make([]byte, 8)
 	binary.BigEndian.PutUint64(bz, count)
-	store.Set([]byte{1}, bz)
+	countStore := prefix.NewStore(ctx.KVStore(storeKey), types.KeyPrefix(types.ActivatedTACounterPrefix))
+	countStore.Set([]byte{1}, bz)
 
 	return nil
 }
